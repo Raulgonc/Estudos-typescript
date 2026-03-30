9ccaf2ceb375dfd6b606e121f70a3c20faee05b5
Raulgonc <raulgonc4@gmail.com>
2026-03-30 09:19:19 -0300

    Configurar TypeScript com tsconfig.json

---
+++ dist/js/byteBank.js
@@ -0,0 +1,37 @@
+"use strict";
+let saldo = 3000;
+const elementoSaldo = document.querySelector(".saldo-valor .valor");
+elementoSaldo.textContent = saldo.toString();
+const elementoFormulario = document.querySelector(".block-nova-transacao form");
+elementoFormulario.addEventListener("submit", function (event) {
+    event.preventDefault();
+    if (!elementoFormulario.checkValidity()) {
+        alert("Preencha todos os campos corretamente!");
+        return;
+    }
+    const inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao");
+    const inputValor = elementoFormulario.querySelector("#valor");
+    const inputData = elementoFormulario.querySelector("#data");
+    let tipoTransacao = inputTipoTransacao.value;
+    let valor = (inputValor.valueAsNumber);
+    let data = new Date(inputData.value);
+    if (tipoTransacao === "Depósito") {
+        saldo += valor;
+    }
+    else if (tipoTransacao === "Transferência" ||
+        tipoTransacao === "Pagamento de Boleto") {
+        saldo -= valor;
+    }
+    else {
+        alert("Tipo de transação inválida!");
+        return;
+    }
+    elementoSaldo.textContent = saldo.toString();
+    const novaTransacao = {
+        tipoTransacao: tipoTransacao,
+        valor: valor,
+        data: data,
+    };
+    console.log(novaTransacao);
+    elementoFormulario.reset();
+});
